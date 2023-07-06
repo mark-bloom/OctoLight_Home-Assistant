d8fb8f018ab062b8fa57a9548e179913037b526e
Morten Lindhardt <r3Fuze@gmail.com>
2023-07-06 12:46:53 +0200

    Fix error

--- octoprint_octolightHA/__init__.py
+++ octoprint_octolightHA/__init__.py
@@ -215,19 +215,6 @@ class OctoLightHAPlugin(
 
 	def get_update_information(self):
 		return False
-		 return dict(
-		 	octolightHA=dict(
-		 		displayName="OctoLightHA",
-		 		displayVersion=self._plugin_version,
-
-		 		type="github_release",
-		 		current=self._plugin_version,
-
-		 		user="mark.bloom",
-		 		repo="OctoLightHA",
-		 		pip="https://github.com/mark-bloom/OctoLight_Home-Assistant/archive/{target}.zip"
-		 	)
-		 )
 
 	def register_custom_events(self):
 		return ["light_state_changed"]
